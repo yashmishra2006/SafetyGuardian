3f78d64dfe2c9591369ff7c06b274a938201647a
YASH MISHRA <yash.48725095@gmail.com>
2025-04-25 17:17:26 +0530

    updates

--- backend/app.py
+++ backend/app.py
@@ -367,13 +367,6 @@ def get_result(analysis_id):
         'result': analysis_results[analysis_id]
     })
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve_react_app(path):
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
